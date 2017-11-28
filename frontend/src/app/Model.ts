7effd070fcf3b8c4881e68cd62b1cfb1c2947280
Carlo Field <cfi@bluesky-it.ch>
2017-11-28 16:29:26 +0100

    Add some basic values to Models
    Signed-off-by: Carlo Field <cfi@bluesky-it.ch>

--- frontend/src/app/Model.ts
+++ frontend/src/app/Model.ts
@@ -1,15 +1,74 @@
-export interface Character {
+export class Character {
+    name: string;
+    health: number;
+    armorClass: number;
+    race: Race;
+    class: CharacterClass;
+    spells: Spell[];
+    preparedSpells: Spell[];
+    constitution: Stat;
+    wisdom: Stat;
+    intelligence: Stat;
+    strength: Stat;
+}
+
+export class Race {
+    healthDie: Die;
+}
+
+export class CharacterClass {
 
 }
 
-export interface Race {
+export class Spell {
 
 }
 
-export interface Class {
+export class Stat {
+    private _value: number;
 
+    constructor(value: number) {
+        this._value = value;
+    }
+
+    get value(): number {
+        return this._value;
+    }
+
+    set value(value: number) {
+        this._value = value;
+    }
+
+    get modifier(): number {
+        return Math.floor((value - 10) / 2);
+    }
 }
 
-export interface Spell {
+export class Die {
+    //Format: MultiplierXSize: 1d6
+    multiplier: number;
+    size: number;
+
+    constructor(multiplier: number, size: number) {
+        this.multiplier = multiplier;
+        this.size = size;
+    }
+
+    constructor(die: string) {
+        let values = die.split('x');
+        this.multiplier = values[0];
+        this.size = values[1];
+    }
+
+    roll() {
+        let total = 0;
+        for(let counter = 0; counter < this.multiplier; counter++) {
+            total += this.random(this.size);
+        }
+    }
+
+    private random(max) {
+        return Math.floor(Math.random() * ((max-1)+1)+1)
+    }
 
 }
